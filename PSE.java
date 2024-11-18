49ce87ed09748651bb3ab630c666c5f0723e4d9c
Noriel Omar Fabella <152840848+NorielFabella@users.noreply.github.com>
2024-11-18 06:12:44 +0000

    Final commit

--- PSE.java
+++ PSE.java
@@ -1,25 +1,25 @@
 public class PSE {
     public static void main(String[] args) {
 
-        Subscriber alice = new Subscriber("Alice");
-        Subscriber bob = new Subscriber("Bob");
+        Subscriber juan = new Subscriber("Juan");
+        Subscriber john = new Subscriber("John");
         Subscriber charlie = new Subscriber("Charlie");
 
         NewsAgency newsAgency = new NewsAgency();
 
-        newsAgency.attach(alice);
-        newsAgency.attach(bob);
+        newsAgency.attach(juan);
+        newsAgency.attach(john);
         newsAgency.attach(charlie);
         
         System.out.println("Breaking News Published!");
         newsAgency.setLatestNews("Massive storm heading towards the coast!");
 
-        newsAgency.detach(bob);
+        newsAgency.detach(john);
 
         System.out.println("\nNew Breaking News Published!");
         newsAgency.setLatestNews("Government announces new economic policies.");
 
-        newsAgency.attach(bob);
+        newsAgency.attach(john);
 
         System.out.println("\nFinal Breaking News Published!");
         newsAgency.setLatestNews("Scientists discover cure for a major disease!");
